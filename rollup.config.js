cae4d5a40e7fdd25d81600c652a01c3ad6afa26c
rquanx <runquant@gmail.com>
2022-06-26 01:59:37 +0800

    chore: dep fix

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,14 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import configList from "./rollup.input";
 import {
-  eslint
-} from "rollup-plugin-eslint";
-import {
   uglify
 } from "rollup-plugin-uglify";
+import ts from "rollup-plugin-ts";
 
 configList.map((item) => (
   item.plugins = [
+    ts(),
     resolve(),
-    eslint({
-      /* your options */
-    }),
     babel({
       exclude: 'node_modules/**', // 只编译我们的源代码
       plugins: [
